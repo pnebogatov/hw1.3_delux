b6e0ff36073f214290ec19c31ed5b8da244396f0
pnebogatov <pnebogatov@yandex.ru>
2022-02-28 21:33:38 +0700

    first commit

---
+++ Navigation_version99/FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FeedViewController.swift
+//  Navigation_version99
+//
+//  Created by Pavel Nebogatov on 23.02.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ Navigation_version99/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  Navigation_version99
+//
+//  Created by Pavel Nebogatov on 23.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let button = UIButton(frame: CGRect(x:  100, y: 300, width: 200, height: 50))
+        button.setTitle("press me", for: .normal)
+        button.addTarget(self, action: #selector(tap), for: .touchUpInside)
+        view.addSubview(button)
+        
+    }
+
+    @objc func tap(){
+        let vc = PostViewController()
+        navigationController?.pushViewController(vc, animated: true)
+        
+        
+    }
+    
+
+}
+
+
